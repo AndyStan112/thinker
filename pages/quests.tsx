11bd0e6dd3949e30f9bad2e388ab4ad6c7b44184
AndyStan112 <stanica.andrei28@gmail.com>
2023-04-24 17:48:06 +0300

    replace outdated atoms

--- pages/quests.tsx
+++ pages/quests.tsx
@@ -1,36 +1,36 @@
-import Navbar from '@/Components/header/Navbar';
-import { useState, useMemo, useEffect } from 'react';
-import { useAtom } from 'jotai';
-import { currExpAtom } from '@/lib/atoms';
-import { useSession } from 'next-auth/react';
-import { Session } from 'next-auth';
+import Navbar from "@/Components/header/Navbar";
+import { useState, useMemo, useEffect } from "react";
+import { useAtom } from "jotai";
+import { totalExperienceAtom } from "@/lib/atoms";
+import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
 const Task = ({ show, task, getQuests }) => {
   const [open, setOpen] = useState(false);
   const { data: session }: { data: Session | null } = useSession();
-  const [currExp, setCurrExp] = useAtom(currExpAtom);
+  const [totalExperience, setTotalExperience] = useAtom(totalExperienceAtom);
   return show ? (
     <div className="flex flex-col border-y-2 border-amber-200 pl-4 ml-2">
       <div className="flex items-center border-2 hover:border-sky-200">
         <img
-          className={'h-4/6 ' + (task.finished ? 'pointer-events-none' : '')}
+          className={"h-4/6 " + (task.finished ? "pointer-events-none" : "")}
           onClick={() => {
-            fetch('/api/experience/post/' + session.id, {
-              method: 'POST',
+            setTotalExperience(totalExperience + task.experience);
+            fetch("/api/experience/post/" + session.id, {
+              method: "POST",
               headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
               },
               body: JSON.stringify({
                 id: task.id,
-                type: 'task',
+                type: "task",
                 title: task.title,
                 experience: task.experience,
               }),
             })
               .then(() => getQuests())
-              .then(() => setCurrExp(currExp + task.experience))
               .catch((e) => console.log(e));
           }}
-          src={task.finished ? 'check_box.svg' : 'check_box_blank.svg'}
+          src={task.finished ? "check_box.svg" : "check_box_blank.svg"}
         ></img>
         <button
           className="flex items-center w-full ml-1"
@@ -41,19 +41,19 @@ const Task = ({ show, task, getQuests }) => {
           {task.title}
           <p
             className={
-              'ml-auto ' +
-              (task.finished ? 'text-emerald-700' : 'text-slate-500')
+              "ml-auto " +
+              (task.finished ? "text-emerald-700" : "text-slate-500")
             }
           >
             {task.experience} exp
           </p>
           <img
             className=""
-            src={open ? 'expand_less.svg' : 'expand_more.svg'}
+            src={open ? "expand_less.svg" : "expand_more.svg"}
           ></img>
         </button>
       </div>
-      <div className={'ml-4 ' + (!open ? 'hidden' : 'inline')}>
+      <div className={"ml-4 " + (!open ? "hidden" : "inline")}>
         {task.description}
       </div>
     </div>
@@ -70,18 +70,18 @@ const Quest = ({ title, tasks, getQuests }) => {
       tasks
         .filter((task) => task.finished)
         .reduce((acc, task) => acc + task.experience, 0),
-    [tasks],
+    [tasks]
   );
   const totalExp: number = useMemo(
     () => tasks.reduce((acc, task) => acc + task.experience, 0),
-    [tasks],
+    [tasks]
   );
   return (
     <div className="flex flex-col  pl-2">
       <div className="flex items-center border-2 hover:border-sky-200 ">
         <img
           className="h-4/5"
-          src={finished ? 'check_box.svg' : 'check_box_blank.svg'}
+          src={finished ? "check_box.svg" : "check_box_blank.svg"}
         ></img>
         <button
           className="flex items-center w-full ml-1"
@@ -92,14 +92,14 @@ const Quest = ({ title, tasks, getQuests }) => {
           <p>{title} </p>
           <p
             className={
-              'ml-auto ' + (finished ? 'text-emerald-700' : 'text-slate-500')
+              "ml-auto " + (finished ? "text-emerald-700" : "text-slate-500")
             }
           >
             {currExp}/{totalExp} exp
           </p>
           <img
             className=""
-            src={open ? 'expand_less.svg' : 'expand_more.svg'}
+            src={open ? "expand_less.svg" : "expand_more.svg"}
           ></img>
         </button>
       </div>
@@ -118,12 +118,12 @@ const Quests = (props: any) => {
   const { data: session }: { data: Session | null } = useSession();
   const [quests, setQuests] = useState([]);
   const getQuests = () => {
-    fetch('/api/quests/get/' + session.id)
+    fetch("/api/quests/get/" + session.id)
       .then((r) =>
         r.json().then((quests) => {
           setQuests(quests);
           console.log(quests);
-        }),
+        })
       )
       .catch((e) => console.log(e));
   };
