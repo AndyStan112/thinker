89cc003e1d09a00e834019cd36013decd3379886
AndyStan112 <stanica.andrei28@gmail.com>
2023-07-01 19:31:04 +0300

    round first and last elements of quest list

--- Components/quests/quest.tsx
+++ Components/quests/quest.tsx
@@ -32,90 +32,88 @@ const Quest: FC<{
     [quest]
   );
   return (
-    <>
-      <div className="flex flex-col  ">
-        {showAddTask && (
-          <div className="absolute top-28 left-1/2 -translate-x-1/2 w-1/4 mt-8 bg-violet-800 rounded-lg">
-            <AddTask
-              questId={quest.id}
-              setQuests={setQuests}
-              setShowAddTask={setShowAddTask}
-            ></AddTask>
+    <div className="flex flex-col first:rounded-t-lg last:rounded-b-lg ">
+      {showAddTask && (
+        <div className="absolute top-28 left-1/2 -translate-x-1/2 w-1/4 mt-8 bg-violet-800 rounded-lg">
+          <AddTask
+            questId={quest.id}
+            setQuests={setQuests}
+            setShowAddTask={setShowAddTask}
+          ></AddTask>
+        </div>
+      )}
+      <div className="flex items-center  border-y-[1.5px]  border-[#8d7cd9] hover:border-solid hover:border-2 hover:border-sky-800 ">
+        <img
+          className="h-10  pl-2"
+          onClick={async () => {
+            if (!canFinish) {
+              setIncompleteError(true);
+              return;
+            }
+            if (finished) return;
+            setTotalExperience(totalExperience + quest.experience);
+            setFinished(true);
+            await fetch("/api/experience/post/" + sessionId, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                id: quest.id,
+                type: "quest",
+                title: quest.title,
+                experience: quest.experience,
+              }),
+            })
+              .then(() => getQuests())
+              .catch((e) => console.log(e));
+          }}
+          src={finished ? "check_box.svg" : "check_box_blank.svg"}
+        ></img>
+        <button
+          className="flex items-center w-full ml-1"
+          onClick={() => {
+            setOpen(open ? false : true);
+          }}
+        >
+          <p>{quest.title} </p>
+          <p
+            className={
+              "ml-auto " +
+              (finished ? "text-fuchsia-600 font-semibold" : "text-slate-500")
+            }
+          >
+            {currQuestExp}/{totalQuestExp} +{quest.experience} exp
+          </p>
+          <img
+            className=""
+            src={open ? "expand_less.svg" : "expand_more.svg"}
+          ></img>
+        </button>
+      </div>
+      <div className="flex flex-col ">
+        {quest.tasks.map((task) => (
+          <Task
+            key={task.id}
+            show={open}
+            task={task}
+            sessionId={sessionId}
+            getQuests={getQuests}
+          />
+        ))}
+        {open && (
+          <div className="flex flex-col w-fit self-center">
+            <button
+              className="ml-2 my-2 flex items-center border-2 rounded-lg bg-purple-400 hover:bg-purple-500 border-purple-600 focus:border-fuchsia-800"
+              onClick={() => setShowAddTask(true)}
+            >
+              <img className="h-10" src="add_quest.svg"></img>
+              <p className="mx-2">Add task</p>
+            </button>
           </div>
         )}
-        <div className="flex items-center border-y-[1.5px] border-[#8d7cd9] hover:border-solid hover:border-2 hover:border-sky-800 ">
-          <img
-            className="h-10  pl-2"
-            onClick={async () => {
-              if (!canFinish) {
-                setIncompleteError(true);
-                return;
-              }
-              if (finished) return;
-              setTotalExperience(totalExperience + quest.experience);
-              setFinished(true);
-              await fetch("/api/experience/post/" + sessionId, {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  id: quest.id,
-                  type: "quest",
-                  title: quest.title,
-                  experience: quest.experience,
-                }),
-              })
-                .then(() => getQuests())
-                .catch((e) => console.log(e));
-            }}
-            src={finished ? "check_box.svg" : "check_box_blank.svg"}
-          ></img>
-          <button
-            className="flex items-center w-full ml-1"
-            onClick={() => {
-              setOpen(open ? false : true);
-            }}
-          >
-            <p>{quest.title} </p>
-            <p
-              className={
-                "ml-auto " +
-                (finished ? "text-fuchsia-600 font-semibold" : "text-slate-500")
-              }
-            >
-              {currQuestExp}/{totalQuestExp} +{quest.experience} exp
-            </p>
-            <img
-              className=""
-              src={open ? "expand_less.svg" : "expand_more.svg"}
-            ></img>
-          </button>
-        </div>
-        <div className="flex flex-col ">
-          {quest.tasks.map((task) => (
-            <Task
-              key={task.id}
-              show={open}
-              task={task}
-              sessionId={sessionId}
-              getQuests={getQuests}
-            />
-          ))}
-          {open && (
-            <div className="flex flex-col w-fit self-center">
-              <button
-                className="ml-2 my-2 flex items-center border-2 rounded-lg bg-purple-400 hover:bg-purple-500 border-purple-600 focus:border-fuchsia-800"
-                onClick={() => setShowAddTask(true)}
-              >
-                <img className="h-10" src="add_quest.svg"></img>
-                <p className="mx-2">Add task</p>
-              </button>
-            </div>
-          )}
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Quest;
